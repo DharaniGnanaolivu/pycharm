0e66e7aa34c0324660db54b0f3e3d1ac527a6747
DharaniGnanaolivu <132817474+DharaniGnanaolivu@users.noreply.github.com>
2023-08-17 15:13:40 +0530

    Add files via upload

---
+++ functional.py
@@ -0,0 +1,12 @@
+import unittest
+def calculate_rectangle_area(length, width):
+    area = length * width
+    return area
+
+# Input from the user
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+
+# Calculate and display the area
+area = calculate_rectangle_area(length, width)
+print(f"The area of the rectangle is: {area}")
