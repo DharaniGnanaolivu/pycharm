b3358e897bf78ea6b19cb20ef8b9ae43b770d5df
DharaniGnanaolivu <132817474+DharaniGnanaolivu@users.noreply.github.com>
2023-08-29 15:19:31 +0530

    Add files via upload

--- task6-1.py
+++ task6-1.py
@@ -1,44 +1,23 @@
 import sqlite3
 
-def create_database():
-    # Connect to or create a new database file
-    conn = sqlite3.connect("mydatabase.db")
+def login_safe(username, password):
+    conn = sqlite3.connect("users.db")
     cursor = conn.cursor()
 
-    # Create a table called "users" with columns: id, username, and email
-    create_table_query = """
-    CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY,
-        username TEXT NOT NULL,
-        email TEXT NOT NULL
-    );
-    """
-    cursor.execute(create_table_query)
-    conn.commit()
+    query = "SELECT * FROM users WHERE username = ? AND password = ?"
+    cursor.execute(query, (username, password))
+
+    user_data = cursor.fetchone()
     conn.close()
 
-    def authenticate(username, password):
-        conn = sqlite3.connect('users.db')
-        cursor = conn.cursor()
+    return user_data
 
-        query = f"SELECT id FROM users WHERE username = '{username}' AND password = '{password}'"
-        cursor.execute(query)
+if __name__ == "__main__":
+    username = input("Enter username: ")
+    password = input("Enter password: ")
 
-        user_id = cursor.fetchone()
-
-        conn.close()
-
-        return user_id
-
-    if __name__ == "__main__":
-        username = input("Enter your username: ")
-        password = input("Enter your password: ")
-
-        user_id = authenticate(username, password)
-
-        if user_id:
-            print("Login successful!")
-        else:
-            print("Login failed.")
-
-
+    user_data = login_safe(username, password)
+    if user_data:
+        print("Login successful.")
+    else:
+        print("Login failed.")
